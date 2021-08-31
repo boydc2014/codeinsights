b46466d08aeadaa3e6692ad6795774e01f15902e
Dong Lei <donglei@microsoft.com>
2021-08-31 12:05:25 +0800

    Update insights based on new index format

--- scripts/insights.js
+++ scripts/insights.js
@@ -1,4 +1,4 @@
-const { path } = require('../utils/pathUtil');
+const path = require('path');
 const fs = require('fs');
 const { exit } = require('process');
 
@@ -8,21 +8,24 @@ const getSubDirNames = (dir) => {
     return fs.readdirSync(dir);
 }
 
-const getIndexNames = () => {
+const getIndexNames = () => 
+{
     return getSubDirNames(indexesDir).filter(x => x !== "test");
 }
 
-var loadIndex = () => {
+var loadIndex = () =>
+{
     const indexNames = getIndexNames();
     if (indexNames.length == 0) {
         console.log("No indexes is found at ./indexes, did you forget to run 'node index.js <full path to repo>'? ");
         exit(1);
     }
 
-    if (indexNames.length > 1) {
+    if (indexNames.length > 1)
+    {
         console.log(`Mulitple indexes are found in ./indexes, but we only load the first one ${indexNames[0]}`);
     }
-
+    
     const indexPath = path.join(indexesDir, indexNames[0], 'repo.json');
     if (!fs.existsSync(indexPath)) {
         console.log(`No index file found at ${indexPath}, abort`);
@@ -43,26 +46,32 @@ const QAs = [
         answer: (index) => { return index.solutions.length; }
     },
     {
-        question: "How many projects in the repo? How many c# projects?",
+        question: "How many projects in the repo?",
         answer: (index) => {
             const projects = index.solutions.flatMap(x => x.projects);
-            const csProjects = projects.filter(p => p.type == 'csProject');
-            return `Total projects:${projects.length}, csharp projects: ${csProjects.length}`;
+            return `Total projects:${projects.length}`;
         }
     },
     {
         question: "Which solution contains the most projects? ",
         answer: (i) => {
             const sortedSolutions = i.solutions.sort((a, b) => {
-                return b.projects.filter(p => p.type == 'csProject').length - a.projects.filter(p => p.type == 'csProject').length;
+                return b.projects.length - a.projects.length;
             });
-            const top = sortedSolutions[0];
-            return `Solution ${top.name} contains ${top.projects.filter(p => p.type == 'csProject').length} c# projects at ${top.path}`;
+            
+            let summary = "";
+            for (let i = 0; i < sortedSolutions.length && i < 10; i++)
+            {
+                const message = `Top ${i+1} Solution ${sortedSolutions[i].name} contains ${sortedSolutions[i].projects.length} projects at ${sortedSolutions[i].path}`;
+                summary = summary.length == 0 ? message : summary + "\r\n   " + message;
+            }
+            return summary;
         }
     }
 ]
 
-QAs.forEach(qa => {
+QAs.forEach(qa => 
+{
     console.log(`Q: ${qa.question}`);
     console.log(`A: ${qa.answer(index)}`);
 });
